703b287dd97f5effb580bd79e0094c612d34e8d6
SMejia115 <s.mejia6@utp.edu.co>
2023-07-14 23:38:24 -0500

    Directorios

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,14 @@
 import express from "express";
-// import path, { dirname } from "path";
+//import path, { dirname } from "path";
 import bodyParser from "body-parser";
 import cors from 'cors'
 // import { error } from "console";
 
 import indexRoutes from "./routes/routes.js"
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 //const PORT = 9000;
@@ -12,7 +16,11 @@ const PORT = process.env.PORT || 9000;
 
 app.use(bodyParser.json())
 app.use(cors())
+
+app.set('routes' , join(__dirname, 'routes'))
 app.use(indexRoutes)
+
+app.use(express.static(join(__dirname, '../public')))
 
 
 app.listen(PORT, () => {
